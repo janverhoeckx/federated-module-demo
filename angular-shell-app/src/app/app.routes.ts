34924d130c87103536e89876639fbb3bb54b2f18
Jan Verhoeckx <janverhoeckx@gmail.com>
2024-11-13 21:59:36 +0100

    added federated module config for shell app

--- angular-shell-app/src/app/app.routes.ts
+++ angular-shell-app/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {WebComponentWrapper, WebComponentWrapperOptions} from '@angular-architects/module-federation-tools';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'react',
+    component: WebComponentWrapper,
+    data: {
+      type: 'script',
+      remoteEntry: 'http://localhost:3000/remoteEntry.js',
+      remoteName: 'react',
+      exposedModule: './web-components',
+      elementName: 'react-element'
+    } as WebComponentWrapperOptions
+  }
+];
